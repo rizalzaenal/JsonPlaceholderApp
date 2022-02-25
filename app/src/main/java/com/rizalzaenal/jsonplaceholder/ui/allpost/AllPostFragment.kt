8b32ad7c6327845f51c9baa3f2b05d32beea741f
rizalzaenal <rizaenalofficial@gmail.com>
2022-02-26 03:12:23 +0700

    create layout for all post screen

---
+++ app/src/main/java/com/rizalzaenal/jsonplaceholder/ui/allpost/AllPostFragment.kt
@@ -0,0 +1,16 @@
+package com.rizalzaenal.jsonplaceholder.ui.allpost
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.rizalzaenal.jsonplaceholder.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AllPostFragment: Fragment(R.layout.fragment_users_post) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/rizalzaenal/jsonplaceholder/ui/allpost/AllPostViewModel.kt
@@ -0,0 +1,8 @@
+package com.rizalzaenal.jsonplaceholder.ui.allpost
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class AllPostViewModel: ViewModel() {
+}
